bd27ac33fe62b53ccec2b0a55bc780999d26d8da
Chandra Mouli <chandramouli7616@gmail.com>
2025-09-21 15:52:30 +0530

    git rebase -i HEAD~5

--- aks/main.tf
+++ aks/main.tf
@@ -1,12 +1,12 @@
-resource "azurerm_resource_group" "aks" {
-  name     = var.resource_group_name
-  location = var.location
-}
+# resource "azurerm_resource_group" "aks" {
+#   name     = var.resource_group_name
+#   location = var.location
+# }
 
 # resource "azurerm_storage_account" "storage_account" {
-#   name                     = "my_aks_storage_tf"
-#   resource_group_name      = var.resource_group_name
-#   location                 = var.location
+#   name                     = "myaksstoragetf" # Storage account names must be between 3 and 24 characters in length and may contain numbers and lowercase letters only.
+#   resource_group_name      = azurerm_resource_group.aks.name
+#   location                 = azurerm_resource_group.aks.location
 #   account_tier             = "Standard"
 #   account_replication_type = "GRS"
 
@@ -17,14 +17,14 @@ resource "azurerm_resource_group" "aks" {
 
 # data "azurerm_storage_blob" "blob_container" {
 #   name                   = "my-aks-blob-container"
-#   storage_account_name   = azurerm_storage_account.storage_account.name #"example-storage-account-name"
-#   storage_container_name = "my_aks_blob_container_tf"
+#   storage_account_name   = azurerm_storage_account.storage_account.name
+#   storage_container_name = "myaksblobcontainertf" # The name of the container within the storage account
 # }
 
 resource "azurerm_kubernetes_cluster" "aks" {
   name                = var.aks_cluster_name
-  location            = azurerm_resource_group.aks.location
-  resource_group_name = azurerm_resource_group.aks.name
+  location            = var.location  #azurerm_resource_group.aks.location
+  resource_group_name = var.resource_group_name  #azurerm_resource_group.aks.name
   dns_prefix          = "${var.aks_cluster_name}-dns"
 
   default_node_pool {
