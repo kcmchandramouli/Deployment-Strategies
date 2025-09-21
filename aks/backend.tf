323c7d39d5b3c3066c9a3e5f4ae1637592e5579d
Chandra Mouli <chandramouli7616@gmail.com>
2025-09-21 13:44:09 +0530

    Testing blob container creation

--- aks/backend.tf
+++ aks/backend.tf
@@ -2,7 +2,7 @@ terraform {
   backend "azurerm" {
     resource_group_name  = "my-aks-rg"
     storage_account_name = "my_aks_storage_tf"
-    container_name       = "my_aks_container_tf"
+    container_name       = "my_aks_blob_container_tf"
     key                  = "terraform.tfstate"   # will change for stage
   }
 }
--- aks/main.tf
+++ aks/main.tf
@@ -3,6 +3,24 @@ resource "azurerm_resource_group" "aks" {
   location = var.location
 }
 
+resource "azurerm_storage_account" "storage_account" {
+  name                     = "my_aks_storage_tf"
+  resource_group_name      = var.resource_group_name
+  location                 = var.location
+  account_tier             = "Standard"
+  account_replication_type = "GRS"
+
+  tags = {
+    environment = "staging"
+  }
+}
+
+data "azurerm_storage_blob" "blob_container" {
+  name                   = "my-aks-blob-container"
+  storage_account_name   = azurerm_storage_account.storage_account.name #"example-storage-account-name"
+  storage_container_name = "my_aks_blob_container_tf"
+}
+
 resource "azurerm_kubernetes_cluster" "aks" {
   name                = var.aks_cluster_name
   location            = azurerm_resource_group.aks.location
